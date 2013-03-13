0cee5b80f19573c64d41702a61f58d7643a0f86b
Hans Engel <engel@engel.uk.to>
2013-03-13 12:43:18 -0700

    Add "call" action

--- assets/www/js/please/actions.js
+++ assets/www/js/please/actions.js
@@ -2,6 +2,11 @@
  * Actions triggered by the Butler API
  */
 cordova.define('please/actions', function(require, exports, module) {
+    var PhoneCall = require('cordova/plugin/phonecall');
+    exports.call = function(payload) {
+        PhoneCall.call(payload);
+    };
+
     exports.web = function(payload) {
         window.open(payload);
     };
