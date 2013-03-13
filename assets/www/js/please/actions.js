767256cd05917f7f1106a4dfd3c7964ee587110d
Hans Engel <engel@engel.uk.to>
2013-03-13 12:43:50 -0700

    Add calendar plugin + action

--- assets/www/js/please/actions.js
+++ assets/www/js/please/actions.js
@@ -2,7 +2,14 @@
  * Actions triggered by the Butler API
  */
 cordova.define('please/actions', function(require, exports, module) {
+    var Calendar = require('cordova/plugin/calendar');
     var PhoneCall = require('cordova/plugin/phonecall');
+
+    exports['calendar.addEvent'] = function(payload) {
+        Calendar.createEvent(payload.title, payload.location, payload.notes,
+                             payload.startDate, payload.endDate);
+    };
+
     exports.call = function(payload) {
         PhoneCall.call(payload);
     };
