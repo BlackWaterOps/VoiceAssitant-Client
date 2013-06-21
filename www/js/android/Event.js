ce935d4a73eabbca1ac5e5780b3556ce1268bd08
jrschifa <jeffs@stremor.com>
2013-06-20 17:00:53 -0700

    add EventPlugin

---
+++ www/js/android/Event.js
@@ -0,0 +1,50 @@
+function eventPlugin() {}
+
+eventPlugin.prototype.addEvent = function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+  if (typeof errorCallback != "function") {
+    console.log("eventPlugin.addEvent failure: errorCallback parameter must be a function");
+    return
+  }
+
+  if (typeof successCallback != "function") {
+    console.log("eventPlugin.addEvent failure: successCallback parameter must be a function");
+    return
+  }
+
+  cordova.exec(
+  successCallback, // called when signature capture is successful
+  errorCallback, // called when signature capture encounters an error
+  'EventPlugin', // Tell cordova that we want to run "PushNotificationPlugin"
+  'addEvent', // Tell the plugin the action we want to perform
+  [{
+    "title": title,
+    "description": notes,
+    "eventLocation": location,
+    "startTimeMillis": startDate.getTime(),
+    "endTimeMillis": endDate.getTime()
+  }]); // List of arguments to the plugin
+};
+calendarPlugin.prototype.deleteEvent = function(title, location, notes, startDate, endDate, deleteAll, successCallback, errorCallback) {
+  throw "NotImplemented";
+}
+
+calendarPlugin.prototype.findEvent = function(title, location, notes, startDate, endDate, successCallback, errorCallback) {
+  throw "NotImplemented";
+}
+
+calendarPlugin.prototype.modifyEvent = function(title, location, notes, startDate, endDate, newTitle, newLocation, newNotes, newStartDate, newEndDate, successCallback, errorCallback) {
+  throw "NotImplemented";
+}
+
+
+eventPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.eventPlugin = new eventPlugin();
+  return window.plugins.eventPlugin;
+};
+
+
+cordova.addConstructor(eventPlugin.install);
