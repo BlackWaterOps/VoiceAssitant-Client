6474f3f211554f508f4726c4a582d3c75194a22b
Hans Engel <engel@engel.uk.to>
2013-12-27 14:02:55 -0700

    Empty out MainActivity

--- Android/Java/Plexi/Plexi/src/main/java/com/stremor/plexi/MainActivity.java
+++ Android/Java/Plexi/Plexi/src/main/java/com/stremor/plexi/MainActivity.java
@@ -9,21 +9,11 @@ import android.app.Activity;
 import android.os.IBinder;
 import android.view.Menu;
 
-import com.stremor.plexi.interfaces.IPlexiService;
-import com.stremor.plexi.util.MyTest;
-import com.stremor.plexi.util.PlexiService;
-
 public class MainActivity extends Activity {
-    MyTest test;
-
-    IPlexiService plexiService;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 
     @Override
@@ -32,33 +22,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Intent intent = new Intent(this, MyTest.class);
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
-    }
-
-
-    private ServiceConnection mConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            // MyTest.LocalBinder binder = (MyTest.LocalBinder) iBinder;
-
-            // test = binder.getService();
-
-            // plexiService = test.new PlexiCore();
-
-            PlexiService.LocalBinder binder = (PlexiService.LocalBinder) iBinder;
-
-            plexiService = binder.getService();
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-
-        }
-    };
 }
