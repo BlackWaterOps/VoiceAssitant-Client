b342f2591c485cbf81a312890f62494e725e0c37
Hans Engel <engel@engel.uk.to>
2013-12-27 15:09:14 -0700

    Add note for improvement in Plexi client

--- Android/Java/Plexi/Plexi/src/main/java/com/stremor/plexi/models/ShowModel.java
+++ Android/Java/Plexi/Plexi/src/main/java/com/stremor/plexi/models/ShowModel.java
@@ -3,7 +3,9 @@ package com.stremor.plexi.models;
 import com.google.gson.JsonObject;
 
 /**
- * Created by jeffschifano on 10/29/13.
+ * Describes content to be displayed to the user.
+ *
+ * TODO Make more strongly typed (don't throw JSON objects through a public interface!)
  */
 public class ShowModel {
     private JsonObject simple;
