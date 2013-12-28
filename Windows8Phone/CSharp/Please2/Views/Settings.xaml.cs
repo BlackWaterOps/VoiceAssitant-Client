d9de4449acc5dce399d3a0f52d184a306292b5c9
jrschifa <jeffs@stremor.com>
2013-12-28 16:22:31 -0700

    toggle between auth and deauth on action

--- Windows8Phone/CSharp/Please2/Views/Settings.xaml.cs
+++ Windows8Phone/CSharp/Please2/Views/Settings.xaml.cs
@@ -8,11 +8,13 @@ using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using System.Windows.Threading;
 
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
 using Please2.Models;
+using Please2.Util;
 using Please2.ViewModels;
 
 namespace Please2.Views
@@ -24,17 +26,22 @@ namespace Please2.Views
             InitializeComponent();
         }
 
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
         private void Provider_Tap(object sender, System.Windows.Input.GestureEventArgs e)
         {
             ResourceManager resx = new ResourceManager("Plexi.Resources.PlexiResources", Assembly.Load("Plexi"));
 
-            string endpoint = resx.GetString("Authorization");
-
             ProviderModel provider = (sender as FrameworkElement).DataContext as ProviderModel;
 
-            string auth = String.Format("{0}/{1}", endpoint, provider.endpointName);
+            string endpoint = (provider.status == AccountStatus.NotConnected) ? resx.GetString("Authorization") : resx.GetString("Deauthorization");
 
-            string url = String.Format(ViewModelLocator.ChildBrowserUri, auth, false);
+            string auth = String.Format("{0}/{1}", endpoint, provider.name.ToString().ToLower());
+
+            string url = String.Format(ViewModelLocator.ChildBrowserUri, auth, provider.name);
 
             NavigationService.Navigate(new Uri(url, UriKind.Relative));
         }
