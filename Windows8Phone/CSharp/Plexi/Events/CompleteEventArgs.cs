45f7310b0e6cbb25749a8ef2ffd7f5e350fc9329
jrschifa <jeffs@stremor.com>
2013-12-28 16:16:50 -0700

    add complete event obj which is a replacement for the old actor event obj

---
+++ Windows8Phone/CSharp/Plexi/Events/CompleteEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Plexi.Models;
+
+namespace Plexi.Events
+{
+    public class CompleteEventArgs : EventArgs
+    {
+        public ActorModel actor;
+
+        public CompleteEventArgs(ActorModel actor)
+        {
+            this.actor = actor;
+        }
+    }
+}
