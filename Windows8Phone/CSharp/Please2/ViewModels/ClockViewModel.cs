33fcfa7e03dd5a99faba9fcd34111a3edc794cc9
jrschifa <jeffs@stremor.com>
2014-01-06 10:35:31 -0700

    add new view models

---
+++ Windows8Phone/CSharp/Please2/ViewModels/ClockViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Please2.Util;
+
+namespace Please2.ViewModels
+{
+    public class ClockViewModel : GalaSoft.MvvmLight.ViewModelBase
+    {
+        public ColorScheme Scheme { get { return ColorScheme.Default; } }
+
+        private DateTime time;
+        public DateTime Time
+        {
+            get { return time; }
+            set
+            {
+                time = value;
+                RaisePropertyChanged("Time");
+            }
+        }
+    }
+}
---
+++ Windows8Phone/CSharp/Please2/ViewModels/FuelDetailsViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Device.Location;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Maps.Controls;
+using Microsoft.Phone.Maps.Toolkit;
+using Microsoft.Phone.Maps.Services;
+using Microsoft.Phone.Shell;
+
+using GalaSoft.MvvmLight.Command;
+
+using LinqToVisualTree;
+
+using Please2.Models;
+using Please2.Util;
+
+using Plexi;
+
+namespace Please2.ViewModels
+{
+    class FuelDetailsViewModel : DetailsViewModel
+    {
+        public RelayCommand FuelDirectionsLoaded { get; set; }
+
+        public FuelDetailsViewModel(INavigationService navigationService, IPlexiService plexiService) 
+            : base(navigationService, plexiService)
+        {
+            AttachEventHandlers();
+        }
+
+        private void AttachEventHandlers()
+        {
+            PinToStartCommand = new RelayCommand(PinToStart);
+            ShowFullMapCommand = new RelayCommand(ShowFullMap);
+
+            FuelDirectionsLoaded = new RelayCommand(AddDirectionsMap);
+        }
+
+        private void PinToStart()
+        {
+            AltFuelModel fuel = CurrentItem as AltFuelModel;
+
+            Uri uri = new Uri(String.Format("/Views/Details.xaml?id={0}", fuel.id));
+            string title = fuel.station_name;
+            string content = fuel.address;
+            
+            base.PinToStart(uri, title, content, null);
+        }
+
+        private void AddDirectionsMap()
+        {
+            var currentPage = ((App.Current.RootVisual as PhoneApplicationFrame).Content as PhoneApplicationPage);
+
+            var map = currentPage.Descendants<Map>().Cast<Map>().FirstOrDefault();
+
+            if (map != null)
+            {
+                var item = CurrentItem as AltFuelModel;
+
+                var layer = base.CreateMapLayer(item.latitude, item.longitude);
+
+                map.Layers.Add(layer);
+                map.Center = new GeoCoordinate(item.latitude, item.longitude);
+            }
+        }
+
+        private async void ShowFullMap()
+        {
+            AltFuelModel fuel = CurrentItem as AltFuelModel;
+            
+            GeoCoordinate geo = new GeoCoordinate(fuel.latitude, fuel.longitude);
+            string title = fuel.station_name;
+
+            await base.ShowFullMap(title, geo);
+        }
+    }
+}
