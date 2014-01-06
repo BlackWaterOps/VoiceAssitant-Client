afd31af1994a2d53a79a53c75d9124fa995dcfd8
jrschifa <jeffs@stremor.com>
2014-01-06 10:40:29 -0700

    uncomment account helper line

--- Windows8Phone/CSharp/Please2/App.xaml.cs
+++ Windows8Phone/CSharp/Please2/App.xaml.cs
@@ -71,7 +71,7 @@ namespace Please2
         // This code will not execute when the application is reactivated
         private void Application_Launching(object sender, LaunchingEventArgs e)
         {
-            //Please2.Util.AccountHelper.Default.Launching();
+            Please2.Util.AccountHelper.Default.Launching();
         }
 
         // Code to execute when the application is activated (brought to foreground)
