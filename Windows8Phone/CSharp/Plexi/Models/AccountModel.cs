b956acfb0e0238b07f68e2f67b6b3c8590aefdc4
jrschifa <jeffs@stremor.com>
2014-01-09 09:55:02 -0700

    add account model

---
+++ Windows8Phone/CSharp/Plexi/Models/AccountModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plexi.Models
+{
+    public class AccountModel
+    {
+        public string service_name { get; set; }
+        public long id { get; set; }
+    }
+}
