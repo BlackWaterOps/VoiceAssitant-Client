c06420bbdd84b417110b6ea44a1bf4c3baa7b8e0
jrschifa <jeffs@stremor.com>
2014-01-21 16:52:15 -0700

    finish clock/time actor

--- Windows8Phone/CSharp/Please2/ViewModels/ClockViewModel.cs
+++ Windows8Phone/CSharp/Please2/ViewModels/ClockViewModel.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using GalaSoft.MvvmLight;
+using TimeZones;
 
 using Please2.Util;
 
 namespace Please2.ViewModels
 {
-    public class ClockViewModel : GalaSoft.MvvmLight.ViewModelBase
+    public class ClockViewModel : ViewModelBase
     {
         public ColorScheme Scheme { get { return ColorScheme.Default; } }
 
@@ -20,6 +24,62 @@ namespace Please2.ViewModels
             {
                 time = value;
                 RaisePropertyChanged("Time");
+            }
+        }
+
+        private string timeZoneName;
+        public string TimeZoneName
+        {
+            get { return timeZoneName; }
+            set
+            {
+                timeZoneName = value;
+                RaisePropertyChanged("TimeZoneName");
+            }
+        }
+
+        public bool ObservesDST { get; set; }
+
+        public void SetDateTime(string offset)
+        {
+            SetDateTime(Convert.ToDouble(offset));
+        }
+
+        public void SetDateTime(double offset)
+        {
+            SetDateTime(TimeSpan.FromHours(offset));
+        }
+
+        public void SetDateTime(TimeSpan offset)
+        {
+            var zones = TimeZoneService.AllTimeZones;
+
+            ITimeZoneEx zone = zones.Where(x => x.BaseUtcOffset == offset).FirstOrDefault();
+
+            Debug.WriteLine(zone.DaylightName);
+
+            if (zone != null)
+            {
+                DateTime utcDateTime = DateTime.UtcNow;
+
+                DateTimeOffset utc = DateTimeOffset.UtcNow;
+
+                DateTimeOffset service = TimeZoneService.SpecifyTimeZone(utc, zone);
+
+                DateTimeOffset convert = zone.ConvertTime(utc);
+
+                if (zone.IsDaylightSavingTime(convert) && ObservesDST)
+                {
+                   convert = convert.AddHours(-1);
+                }
+
+                Time = convert.DateTime;
+
+                TimeZoneName = zone.StandardName;
+            }
+            else
+            {
+                // set default or throw error
             }
         }
     }
--- Windows8Phone/CSharp/Please2/Views/Clock.xaml.cs
+++ Windows8Phone/CSharp/Please2/Views/Clock.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Threading;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 
-using NodaTime;
+using TimeZones;
 
 using Please2.ViewModels;
 
@@ -36,37 +36,6 @@ namespace Please2.Views
             timer.Tick += Timer_Tick;
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            base.OnNavigatedTo(e);
-
-            string offset;
-
-            if (NavigationContext.QueryString.TryGetValue("offset", out offset))
-            {
-                try
-                {
-                    //TODO: handle dst   
-                    DateTime utc = DateTime.UtcNow;
-
-                    DateTime locale = utc.AddHours(Convert.ToDouble(offset));
-
-                    vm.Time = locale;
-                }
-                catch (Exception err)
-                {
-                    Debug.WriteLine(err.Message);
-                    vm.Time = DateTime.Now;
-                }
-            }
-            else
-            {
-                vm.Time = DateTime.Now;
-            }
-
-            timer.Start();
-        }
-
         protected void Page_Loaded(object sender, RoutedEventArgs e)
         {
             try
@@ -77,12 +46,15 @@ namespace Please2.Views
 
                 int second = vm.Time.Second;
                 SecondHand.Seek(new TimeSpan(0, 0, second));
-
+                
                 int minute = vm.Time.Minute;
                 LongHand.Seek(new TimeSpan(0, minute, second));
 
                 int hour = vm.Time.Hour;
                 HourHand.Seek(new TimeSpan(hour, minute, second));
+
+                timer.Interval = TimeSpan.FromSeconds(60 - second);
+                timer.Start();
             }
             catch (Exception err)
             {
@@ -92,16 +64,9 @@ namespace Please2.Views
 
         protected void Timer_Tick(object sender, EventArgs e)
         {
-            var timezone = TimeZoneInfo.Local;
+            vm.Time = vm.Time.AddMinutes(1);
 
-            var name = timezone.DisplayName.Substring(timezone.DisplayName.IndexOf(")")+1).Trim();
-
-            if (name != null && name != "")
-            {
-                name += " Time";
-            }
-
-            TimeTextBlock.Text = String.Format("{0} {1}", vm.Time.ToString("h:mm tt "), name);
+            timer.Interval = TimeSpan.FromMinutes(1);
         }
     }
 }
